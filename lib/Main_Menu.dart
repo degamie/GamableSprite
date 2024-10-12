dd177d4cac05b7784f1a12a2ce455fdd4679de52
Degamie_sign <degamiesign@gmail.com>
2024-10-12 18:01:18 +0530

    Initial commit

---
+++ lib/Main_Menu.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainMenu extends StatelessWidget{
+  const MainMenu({Key? key}):super(Key:Key);
+  Widget build(BuildContext context){
+    Scaffold(
+      body:Center(
+        child:Column(
+          children: [
+            Text:("GamableSprite_Space"),
+          ],
+          ElevatedButton(
+            onPressed: (){},
+            child:Text("Settings")
+              Padding(
+              padding: const EdgeInsects.all(Vertical:50),
+              child: Text("GamableSprite_Space"),
+          )
+          )
+        )
+      )
+    )
+  }
+}
