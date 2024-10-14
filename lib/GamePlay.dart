2907789014f2037ce10f496d6f873edd8642babc
Degamie_sign <degamiesign@gmail.com>
2024-10-14 22:05:52 +0530

    First Commit

---
+++ lib/GamePlay.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class GamePlay extends StatelessWidget{
+  const GamePlay({Key ?Key});
+  GameWidget(gamableSprite) {return Key;}
+
+  @override
+  Widget build(BuildContext context) {
+    return GameWidget(GamePlay());
+  }
+
+  //
+
+
+}
--- lib/Main_Menu.dart
+++ lib/Main_Menu.dart
@@ -2,24 +2,32 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainMenu extends StatelessWidget{
-  const MainMenu({Key? key}):super(Key:Key);
+  const MainMenu({Key? key}):super(key:key);
   Widget build(BuildContext context){
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: context)=>const GamePlay() as Route<Object?>);
     Scaffold(
       body:Center(
         child:Column(
           children: [
             Text:("GamableSprite_Space"),
-          ],
-          ElevatedButton(
-            onPressed: (){},
-            child:Text("Settings")
-              Padding(
-              padding: const EdgeInsects.all(Vertical:50),
-              child: Text("GamableSprite_Space"),
-          )
-          )
-        )
-      )
+    ElevatedButton(
+    onPressed: (){},
+    child:Text("Settings")
+    Padding(
+    padding: const EdgeInsects.all(Vertical:50),
+    child: Text("GamableSprite_Space"),
     )
+    )
+
+    ElevatedButton(
+    onPressed: (){},
+    child:Text('Options'),
+    )
+    )
+    )
+    )
+    ],
+
   }
 }
