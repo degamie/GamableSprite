c82e7fcf46b14a67ed2474e2199fa758d7a4ab9b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2024-10-21 09:43:54 +0530

    Update Main_Menu.dart

--- lib/Main_Menu.dart
+++ lib/Main_Menu.dart
@@ -15,7 +15,20 @@ class MainMenu extends StatelessWidget{
             child:Text("Settings")
               Padding(
               padding: const EdgeInsects.all(Vertical:50),
-              child: Text("GamableSprite_Space"),
+              child: Text("GamableSprite_Space",
+                    style:Theme.of(context),
+                    .textTheme
+                    .bodytext1
+                    ?.copywith(fontSize:55.0),
+              
+              ),
+                    shadows:[
+                      Shadow(
+                        blurRadius:20.0,
+                        color:Colors.white,
+                        Offset:Offset(0,0),
+                      )
+                    ]
                 mainAxisAlignment:MainAxisAlignment.center,
               
           )
